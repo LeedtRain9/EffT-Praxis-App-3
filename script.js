e24e973a31b44cecc088a5d9e3bd4fb62b7ba593
LeedtRain9 <hendrick.neef@gmx.de>
2025-04-07 20:18:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -589,7 +589,7 @@ function displayOverallScore() {
 
     // Sound-Logik (nur bei Platin) - OHNE AUTOPLAY
     if (medal === 'Platin') {
-        const platinSound = new Audio('Die Flippers.mp3');
+        const platinSound = new Audio('Moskau im Regen.mp3');
         
         // Sound-Button
         const soundButton = document.createElement('button');
